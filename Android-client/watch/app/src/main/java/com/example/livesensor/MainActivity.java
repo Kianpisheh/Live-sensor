f8f8ef85482c352501caa2c695bdeda119b90230
Mohammad Kianpisheh <mkian1368@gmail.com>
2019-12-27 02:33:16 -0500

    the watch version ui completed

--- Android-client/watch/app/src/main/java/com/example/livesensor/MainActivity.java
+++ Android-client/watch/app/src/main/java/com/example/livesensor/MainActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.wearable.activity.WearableActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
@@ -12,26 +17,59 @@ public class MainActivity extends WearableActivity {
     final static private String TAG = "";
 
     private int serverPort = 9000;
-    private String serverAddress = "100.64.81.146";
+    private String serverAddress = "192.168.0.24";
 
     private SocketThread socketThread;
     private MainHandler mainHandler = new MainHandler();
 
 
     // layout elements
-    private EditText portTextEdit, ipTextEdit;
-    private TextView connectionStatus;
-    private SwitchMaterial connectionSwitch;
+    private EditText portTextEdit, addressTextEdit;
+    private Switch connectSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
         // Enables Always-on
         setAmbientEnabled();
+
+        // the server and port ip address
+        addressTextEdit = findViewById(R.id.address_edit_text);
+        addressTextEdit.setText("192.168.0.24");
+        portTextEdit = findViewById(R.id.port_edit_text);
+        portTextEdit.setText("9000");
+
+        // connect switch
+        connectSwitch = findViewById(R.id.connection_sw);
+        connectSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    serverAddress =  addressTextEdit.getText().toString();
+                    serverPort = Integer.parseInt(portTextEdit.getText().toString());
+                    socketThread = new SocketThread(getApplicationContext(), mainHandler, serverAddress, serverPort);
+                    socketThread.start();
+                } else {
+                    if (socketThread != null) {
+                        socketThread.stopThread();
+                    }
+                }
+            }
+        });
+
+
+//        // connection status text view
+//        connectionStatus = findViewById(R.id.conn_status_tv);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (socketThread != null) {
+            socketThread.stopThread();
+        }
     }
 
 
@@ -42,20 +80,20 @@ public class MainActivity extends WearableActivity {
         public void handleMessage(Message msg) {
             if (msg.what == CONN_STATUS_CHANGED) {
                 if (socketThread == null) {
-                    connectionStatus.setText(R.string.disconnected);
-                    connectionSwitch.setChecked(false);
+                    //connectionStatus.setText(R.string.disconnected);
+                    connectSwitch.setChecked(false);
                 } else {
                     if (socketThread.mSocket == null) {
-                        connectionStatus.setText(R.string.disconnected);
-                        connectionSwitch.setChecked(false);
+                        //connectionStatus.setText(R.string.disconnected);
+                        connectSwitch.setChecked(false);
 
                     } else {
                         if (socketThread.mSocket.connected()) {
-                            connectionStatus.setText(R.string.connected);
-                            connectionSwitch.setChecked(true);
+                            //connectionStatus.setText(R.string.connected);
+                            connectSwitch.setChecked(true);
                         } else {
-                            connectionStatus.setText(R.string.disconnected);
-                            connectionSwitch.setChecked(false);
+                            //connectionStatus.setText(R.string.disconnected);
+                            connectSwitch.setChecked(false);
                         }
                     }
                 }
